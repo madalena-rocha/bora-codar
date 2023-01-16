ebbd4a95d52cc2ca08e2f16fda31a77fd6b8975f
Madalena Machado Rocha <rochamada1997m@gmail.com>
2023-01-16 10:30:20 -0300

    add button functionality

---
+++ product-card/script.js
@@ -0,0 +1,24 @@
+const previewImg = document.getElementById("preview-img");
+const productAnimation = document.getElementById("product-animation");
+
+const viewProduct = document.getElementById("view-product");
+const closeView = document.getElementById("close-view");
+
+function openAnimation() {
+    viewProduct.classList.add('hide');
+    closeView.classList.remove('hide');
+    
+    previewImg.classList.add('hide');
+    productAnimation.classList.remove('hide');
+}
+
+function closeAnimation() {
+    viewProduct.classList.remove('hide');
+    closeView.classList.add('hide');
+
+    previewImg.classList.remove('hide');
+    productAnimation.classList.add('hide');
+}
+
+viewProduct.addEventListener("click", openAnimation);
+closeView.addEventListener("click", closeAnimation);
